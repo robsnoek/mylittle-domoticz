90253134aa38efc85fecbedd271f087ebb67bb45
jmleglise <jmleglise@gmail.com>
2016-05-07 14:28:33 +0200

    v1.1
    Log system + Mac Address case insensitive

--- Presence detection (beacon)/check_beacon_presence.py
+++ Presence detection (beacon)/check_beacon_presence.py
@@ -5,33 +5,45 @@
 #   Info: Beacon (BlueTooth Low Energy V4.0) detection and reports back to domoticz
 #   URL : https://github.com/jmleglise/mylittle-domoticz/edit/master/Presence%20detection%20%28beacon%29/check_beacon_presence.py
 #   Version : 1.0
+#   Version : 1.1   Log + Mac Adress case insensitive 
 #
 # Feature : 
 # Check the presence of a list of beacon and update uservariables in Domoticz accordingly. 
 # When the MACADRESS of the beacons are detected, send "HOME". And send "AWAY" when the beacons are not in range.
 # The detection is very fast : around 4 secondes. And the absence is verified every 15 seconds by comparing the hour of the last presence.
 #
-# Ressources :
+# References :
 # https://wiki.tizen.org/wiki/Bluetooth
 # https://storage.googleapis.com/google-code-archive-source/v2/code.google.com/pybluez/source-archive.zip  => pybluez\examples\advanced\inquiry-with-rssi.py
 #
 # Required in Domoticz : An uservariable of type String for each BLE Tag
 #
+# Configuration :
 # Change your IP and Port here :
 URL_DOMOTICZ = 'http://192.168.0.20:8080/json.htm?type=command&param=updateuservariable&idx=PARAM_IDX&vname=PARAM_NAME&vtype=2&vvalue=PARAM_CMD'
 #
 # Configure your Beacon here : 
-# [Name,MacAddress,Timeout,0,idx]
+# [Name,MacAddress,Timeout,0,idx,mode]
 # Name : same as the uservariable in Domoticz
 # Timeout is in secondes the elapsed time  without a detetion for switching the beacon AWAY. Ie :if your beacon emits every 3 to 8 seondes, a timeout of 15 secondes seems good.
 # 0 : used by the script (will keep the time of the last broadcast) 
 # idx of the uservariable in Domoticz for this beacon
+# mode : 0 for 1 update per status change / 1 continuous updating
 
 TAG_DATA = [  
 			["Tag_white","XX:Xx:XX:xx:xx:xx",15,0,8],
 			["Tag_Orange","xx:xx:78:38:xx:xx",15,0,6],
-			["Tag_Green","xx:ff:60:00:xx:xx",15,0,7]
+			["Tag_Green","xx:xx:60:00:xx:xx",15,0,7]
 			]
+
+import logging
+
+# choose between DEBUG (log every information) or CRITICAL (almost no log)
+logLevel=logging.DEBUG
+#logLevel=logging.CRITICAL
+
+#logOutFilename='/var/log/check_beacon_presence.log'       # comment this line to console output
+
 ################ Nothing to edit under this line #####################################################################################
 
 
@@ -45,13 +57,14 @@ import signal
 import threading
 
 
+
 LE_META_EVENT = 0x3e
 OGF_LE_CTL=0x08
 OCF_LE_SET_SCAN_ENABLE=0x000C
 EVT_LE_CONN_COMPLETE=0x01
 EVT_LE_ADVERTISING_REPORT=0x02
 
-def printpacket(pkt):
+def print_packet(pkt):
     for c in pkt:
         sys.stdout.write("%02x " % struct.unpack("B",c)[0])
 
@@ -72,39 +85,45 @@ def handler(signum = None, frame = None):
 for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
     signal.signal(sig, handler)
 
-def RequestThread(idx,cmd, name):
+def request_thread(idx,cmd, name):
     try:
         url = URL_DOMOTICZ
         url=url.replace('PARAM_IDX',str(idx))
         url=url.replace('PARAM_CMD',str(cmd))
         url=url.replace('PARAM_NAME',str(name))
         result = requests.get(url)
-        print url
-        #  logging.debug(" %s -> %s" % (threading.current_thread(), result))
+        logging.debug(" %s -> %s" % (threading.current_thread(), result))
     except requests.ConnectionError, e:
-        print "Request Failed", url
-        #  logging.warning(' %s CONNECTION ERROR %s' % (threading.current_thread(), e) )
+        logging.warning(' %s Request Failed %s - %s' % (threading.current_thread(), e, url) )
 
 class CheckAbsenceThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
     def run(self):
-            time_check_absence = time.time()
+#            time_check_absence = time.time()
             while True:
-                if time.time()>time_check_absence+15 : # check every 15 sec
-                    time_check_absence=time.time()
+                #if time.time()>time_check_absence+15 : # check every 15 sec
+                    #time_check_absence=time.time()
                     for tag in TAG_DATA:
-                        if time.time()>tag[3]+tag[2] and time.time()<tag[3]+2*tag[2]:  #lastseen + timeout
-                            #print time.ctime()," ",tag[0]," send away cmd"
-                            threadReqAway = threading.Thread(target=RequestThread,args=(tag[4],"AWAY",tag[0]))
+                        elapsed_time_absence=tag[3]-time.time()
+                        if elapsed_time_absence>tag[2] and elapsed_time_absence<2*tag[2]:  #upadte only 1 time in the windows [15sec - 30sec]
+                            logging.debug('Tag %s not seen since %i sec => update absence',tag[0],elapsed_time_absence)
+                            threadReqAway = threading.Thread(target=request_thread,args=(tag[4],"AWAY",tag[0]))
                             threadReqAway.start()
+                    time.sleep(15)
 
-dev_id = 0
+devId = 0
+FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if globals().has_key('logOutFilename') :
+    logging.basicConfig(format=FORMAT,filename=logOutFilename,level=logLevel)
+else:
+    logging.basicConfig(format=FORMAT,level=logLevel)
+
 try:
-    sock = bluez.hci_open_dev(dev_id)
-
+    sock = bluez.hci_open_dev(devId)
+    logging.info('Connect to bluetooth device %i',devId)
 except:
-    print "Error accessing bluetooth dongle..."
+    logging.debug('Unable to connect to bluetooth device...')
     sys.exit(1)
 
 old_filter = sock.getsockopt( bluez.SOL_HCI, bluez.HCI_FILTER, 14)
@@ -139,21 +158,22 @@ while True:
                 num_reports = struct.unpack("B", pkt[0])[0]
                 report_pkt_offset = 0
                 for i in range(0, num_reports):
-                            #print time.ctime()
-                            #print "\tUDID: ", printpacket(pkt[report_pkt_offset -22: report_pkt_offset - 6])
-                            #print "\tMAJOR: ", printpacket(pkt[report_pkt_offset -6: report_pkt_offset - 4])
-                            #print "\tMINOR: ", printpacket(pkt[report_pkt_offset -4: report_pkt_offset - 2])
-                            #print "\tMAC address: ", packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
-                            #print "\t(Unknown):", struct.unpack("b", pkt[report_pkt_offset -2]) # don't know what this byte is.  It's NOT TXPower
-                            #print "\tRSSI:", struct.unpack("b", pkt[report_pkt_offset -1]) #  Signal strenght !
+                            #logging.debug('UDID: ', print_packet(pkt[report_pkt_offset -22: report_pkt_offset - 6]))
+                            #logging.debug('MAJOR: ', print_packet(pkt[report_pkt_offset -6: report_pkt_offset - 4]))
+                            #logging.debug('MINOR: ', print_packet(pkt[report_pkt_offset -4: report_pkt_offset - 2]))
+                            #logging.debug('MAC address: ', packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9]))
+                            #logging.debug('Unknown:', struct.unpack("b", pkt[report_pkt_offset -2])) # don't know what this byte is.  It's NOT TXPower ?
+                            #logging.debug('RSSI: %s', struct.unpack("b", pkt[report_pkt_offset -1])) #  Signal strenght !
+                            macAdressSeen=packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9])
+                            logging.debug('Tag Detected %s - RSSI %s - DATA %s', macAdressSeen, struct.unpack("b", pkt[report_pkt_offset -1]),struct.unpack("b", pkt[report_pkt_offset -2])) #  Signal strenght !
                             for tag in TAG_DATA:
-                                if packed_bdaddr_to_string(pkt[report_pkt_offset + 3:report_pkt_offset + 9]) == tag[1]:  # MAC ADDRESS
-                                    #print time.time(),"  ",tag[0]
+                                if macAdressSeen.lower() == tag[1].lower():  # MAC ADDRESS
+                                    logging.debug('It is tag: %s', tag[0])
                                     elapsed_time=int(time.time()-tag[3])  # lastseen
-                                    if elapsed_time>tag[2]: # timeout
-                                        threadReqHome = threading.Thread(target=RequestThread,args=(tag[4],"HOME",tag[0]))
+                                    if elapsed_time>tag[2]: # >timeout
+                                        threadReqHome = threading.Thread(target=request_thread,args=(tag[4],"HOME",tag[0]))
                                         threadReqHome.start()
-                                        #print "send detection",tag[0]
+                                        logging.debug('Tag %s seen after an absence of %i sec : update presence',tag[0],elapsed_time)
                                     tag[3]=time.time()   # update lastseen
                                     
     sock.setsockopt( bluez.SOL_HCI, bluez.HCI_FILTER, old_filter )
